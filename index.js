736f07b22a4b7533f5adb26f74cc85129f7137a0
Jennifer Gifford <jgifford82@gmail.com>
2022-05-21 18:58:25 -0600

    commit tree 13e5cfeea5e532d8cebfedbed6b9a13e0abe4d34

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+// This actually changes the value stored in the variable instead of just returning the capitalized value.
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Voldemort'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not bob';
+}
+
+// Failed attempts to solve second test:
+// function setBestCustomer() {
+//     var bestCustomer = ('not bob');
+//     return bestCustomer;
+// }
+
+// function setBestCustomer(bestCustomer = 'not bob') {
+//     return bestCustomer;
+// }
+
+
+
+// Failed attempts to solve second test.
+// function upperCaseCustomerName(customerName) {
+//     return toUpperCase(customerName);
+// }
+
+// function upperCaseCustomerName(customerName) {
+//     return customerName.toUpperCase();
+// }
+
+// function upperCaseCustomerName(customerName) {
+//     let result = customerName.toUpperCase();
+//     return result;
+// }
+
+// function upperCaseCustomerName() {
+//     let result = customerName.toUpperCase();
+//     return result;
+// }
+
+// function upperCaseCustomerName() {
+//     customerName.toUpperCase();
+// }
+
+// function upperCaseCustomerName() {
+//     return customerName.toUpperCase();
+// }
+
+// function upperCaseCustomerName() {
+//     var customerName = toUpperCase();
+// }
+
+// function upperCaseCustomerName(name) {
+//     var customerName = name.toUpperCase();
+// }
+
+// function upperCaseCustomerName(name) {
+//     var customerName = (name.toUpperCase());
+// }
